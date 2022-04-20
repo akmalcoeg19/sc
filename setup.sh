288a4e8d7609486327cbda0ce08f8a035a3a5bbe
Zanxx <78849151+akmalcoeg19@users.noreply.github.com>
2022-04-21 05:39:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ akbarvpnnnnnnnnnn="raw.githubusercontent.com/myridwan/sc/ipuk/ohp"
 
 # Getting
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://myridwan.github.io/izin | grep $MYIP )
+IZIN=$( curl https://zanxx.xyz/izin | grep $MYIP )
 echo "Memeriksa Hak Akses VPS..."
 if [ $MYIP = $IZIN ]; then
 clear
